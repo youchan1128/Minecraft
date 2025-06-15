f44007183804d2964526c10115afe5c5f3a62b09
sunsss123 <sunsss123@naver.com>
2025-06-15 09:52:18 +0900

    첫 커밋

---
+++ Programming01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Programming01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Add:" + Add(10, 20));
+            ValMain();
+        }
+        static int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+        static void ValMain()
+        {
+            int nScore = 0;
+            float fRat = 1.0f / 4.0f;
+            Console.WriteLine("Score: " + nScore);
+            Console.WriteLine("Rat: " + fRat);
+        }
+    }
+}
