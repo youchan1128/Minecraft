2df70f64e29244323919b3f113b8cf34b0ade057
youchan1128 <youchan071128@naver.com>
2025-06-18 17:53:33 +0900

    01

--- Programming01/obj/Release/net8.0/Programming01.AssemblyInfo.cs
+++ Programming01/obj/Release/net8.0/Programming01.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programming01")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56b2b0e8e0eda135df81bad6fb0d4b1b25de97e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programming01")]
